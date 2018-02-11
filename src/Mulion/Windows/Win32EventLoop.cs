bde4c05d6961c0980f4871685f4e7f24b15eda6d
Yaram <pootisspencerherenao@gmail.com>
2018-02-12 08:36:42 +1100

    Modify Win32 EventLoop backend to use new async abstraction

--- src/Mulion/Windows/Win32EventLoop.cs
+++ src/Mulion/Windows/Win32EventLoop.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Threading.Tasks;
 using static Mulion.Windows.Win32;
 
 namespace Mulion.Windows{
-	class EventLoopBackend : IEventLoopBackend{
-		public Action Quit{private get; set;}
+	class Win32EventLoop : EventLoop{
+		public override Task<Window> CreateWindow(){
+			return Task.FromResult<Window>(new Win32Window());
+		}
 
-		public void PollEvents(){
+		public override void PollEvents(){
 			if(GetMessage(out var message, IntPtr.Zero, 0, 0) != -1){
 				HandleMessage(message);
 			}else{
@@ -13,7 +16,7 @@ namespace Mulion.Windows{
 			}
 		}
 
-		public void RunForever(){
+		public override void RunForever(){
 			while(GetMessage(out var message, IntPtr.Zero, 0, 0) != -1){
 				HandleMessage(message);
 			}
@@ -24,15 +27,9 @@ namespace Mulion.Windows{
 		private void HandleMessage(Message message){
 			TranslateMessage(ref message);
 			DispatchMessage(ref message);
-
-			switch(message.Type){
-				case MessageType.Quit:
-					Quit?.Invoke();
-					break;
-			}
 		}
 
-		public void OnQuit(){
+		public override void Quit(){
 			PostQuitMessage(0);
 		}
 	}
