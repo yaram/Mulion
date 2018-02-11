d68153ac91d7a5b0063375a2ac6bd096b74547d4
Yaram <pootisspencerherenao@gmail.com>
2018-02-12 08:39:00 +1100

    Enable updated Win32 backend

--- src/Mulion/EventLoop.cs
+++ src/Mulion/EventLoop.cs
@@ -1,9 +1,14 @@
 using System;
 using System.Threading.Tasks;
+using Mulion.Windows;
 
 namespace Mulion{
 	public abstract class EventLoop{
 		public static Task<EventLoop> Create(){
+			if(Environment.OSVersion.Platform == PlatformID.Win32NT){
+				return Task.FromResult<EventLoop>(new Win32EventLoop());
+			}
+
 			throw new MulionException("No suitable event loop backend found");
 		}
 
